24c3c39550ee6b1931bfd98a0ef7b2080f0bf4b6
badzso-boop <norbert.ujj@gmail.com>
2025-04-28 09:23:23 +0200

    removed classes

--- BACKEND/BACKEND/Classes/Furniture.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BACKEND.Classes
-{
-    public class Furniture
-    {
-        public string Name { get; set; }
-        public double Width { get; set; }
-        public double Length { get; set; }
-    }
-}

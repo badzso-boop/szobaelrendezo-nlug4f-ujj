3c9102cd23738298410231b368f6074fac101a6b
badzso-boop <norbert.ujj@gmail.com>
2025-05-12 20:23:32 +0200

    mappa atnevezve

--- BACKEND/BACKEND/Classes/RoomRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BACKEND.Classes
-{
-    public class RoomRequest
-    {
-        public double Width { get; set; }
-        public double Length { get; set; }
-        public List<Furniture> Furnitures { get; set; }
-    }
-}
